71110ffea9f7690c31f8dd8439f530737f00e111
yannis staali <yannis.staali@laplateforme.io>
2021-01-19 10:31:05 +0100

    amelioration responsive

--- Vue/vueAccueil.php
+++ Vue/vueAccueil.php
@@ -12,7 +12,7 @@
         <?php include_once 'Vue/includes/header.php'?>
 
             <header>
-                <div class="container container_nav">
+                <div class="container_nav">
                     <div class="site_title">
                         <h1>Umami</h1>
                         <p class="subtitle">A blog exploring new flavor</p>
@@ -42,7 +42,7 @@
                 </article>
             </section>
 
-            <section class="container container_flex container_main">  <!-- CONTAINER DE ARTICLES + ASIDE ------>  
+            <section class="container_main">  <!-- CONTAINER DE ARTICLES + ASIDE ------>  
 
                 <main class="home_main">   <!--MAIN avec les 3 articles a gauche -->
                     <?php foreach ($billets as $billet): ?>
@@ -55,12 +55,13 @@
                             }
                             else echo "<img src='Assets/images/default.jpg'>"; //200 par 160px     
                         ?>
+                        <p class="article_content">Le : <?= $billet['date'] ?></p>
                         </div> <!-- FIN partie image de chaque article \\\\ -->
                         <div class="home_article_each_text"> <!-- partie texte de chaque article -->
                             <a href="<?= "index.php?page=article&id=" . $billet['id'] ?>"><h2 class="article_title"><?= $billet['titre'] ?></h2></a>
-                            <p class="article_content">Le : <?= $billet['date'] ?></p>
                             <p class="article_content">Categorie : <?= $billet['categorie'] ?></p>
                             <p class="article_content"><?= $billet['article'] ?></p>
+                            <a href="<?= "index.php?page=article&id=" . $billet['id'] ?>"><p>Continue reading</p></a>
                         </div> <!--FIN partie texte de chaque article \\\\\\ -->
                     </article>  <!--FIN  partie qui englobe chaque article \\\\\ -->
                     <?php endforeach; ?>
@@ -68,23 +69,26 @@
             
                 <aside class="home_aside">  <!-- PARTIE ASIDE,a propos etc... -->
                     <div class="aside_widget">
-                        <h2 class="widget_title">A propos</h2>
+                        <h3 class="widget_title">A propos</h3>
                         <img src="Assets/images/perso.jpg" alt="" class="widget_image">
-                        <p class="widget_body"></p>
+                        <p class="widget_body">Food and drinks lover, always looking for new taste and savor i'm sharing with you some of my secret recipes.</p>
                     </div>
                     <div class="aside_widget">
-                        <h2 class="widget_title">Work in a team</h2>
+                        <h3 class="widget_title">Work in a team</h3>
                         <div class="widget_collab">
                             <h2 class="widget_collab_title"></h2>
                             <img src="Assets/images/team.jpg" alt="" class="widget_image">
+                            <p class="widget_body">Want to learn how to work in a "brigade" team ? Ask us to book an appointment.</p>
                         </div>
                     <div class="widget_collab">
-                        <h2 class="widget_collab_title">Want to make pizzas ?</h2>
-                        <img src="Assets/images/pizzafume.gif" alt="" class="widget_image">
+                        <h3 class="widget_collab_title">Want to make pizzas ?</h3>
+                        <img src="Assets/images/pizza.jpg" alt="" class="widget_image">
+                        <p class="widget_body">The most emblematic Italian dish revealed with an ancestral italian grandmother's recipe.</p>
                     </div>    
                     <div class="widget_collab">
-                        <h2 class="widget_collab_title">Need a chef to learn ?</h2>
+                        <h3 class="widget_collab_title">Need a chef to learn ?</h3>
                         <img src="Assets/images/chef.jpg" alt="" class="widget_image">
+                        <p class="widget_body">You need someone to take care of your journey into the culinary arts ? Keep us in touch.</p>
                     </div>        
                     </div>
                 </aside> <!--  FIN PARTIE ASIDE ,a propos etc...\\\\ -->  
