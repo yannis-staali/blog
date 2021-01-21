95a40f2b72adefbde2107a65b42585c68d480c0a
yannis staali <yannis.staali@laplateforme.io>
2021-01-21 16:05:08 +0100

    design done before cleaning

--- Vue/vueAccueil.php
+++ Vue/vueAccueil.php
@@ -110,20 +110,20 @@
                         </div>
                     </article>
 
-                    <article class="foot_block">
+                    <article class="foot_block blocko">
                         <h4>About</h4>
                         <a href="index.php">Our strory</a>
                         <a href="index.php">Methodology</a>
                         <a href="index.php">Keep in touch</a>
                     </article>
 
-                    <article class="foot_block">
+                    <article class="foot_block blocko">
                         <h4>Contact</h4>
                         <a href="index.php">Contact us</a>
                         <a href="index.php">Press</a>  
                     </article>
                     
-                    <article class="foot_block">
+                    <article class="foot_block blocko">
                         <h4>Download</h4>
                         <a href="index.php"> Apple Store</a>
                         <a href="index.php">Google Play</a>
